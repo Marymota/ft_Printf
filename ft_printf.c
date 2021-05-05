647b9b8b69e50c2cff6232c44bb4e0a55c2e121d
marymota <dg.marciamota@gmail.com>
2021-05-05 16:08:51 +0100

    Solve %s printed with width

--- ft_printf.c
+++ ft_printf.c
@@ -14,19 +14,18 @@ printf() is called with a "format string" that specifies how to format the value
 */
 //PRINTS
 //PRINTS / CHAR
-int ft_putchr(va_list args)
+int ft_putchr(char c)
 {
-	int c;
-
-	c = va_arg(args, int);
 	return (write(1, &c, 1));
 }
 
-void align(int idx, int w)
+void align(int w)
 {
-	idx = w;
-	while (--idx)
-		write(1, " ", 1);
+	int i;
+
+	i = w;
+	while (--i)
+		ft_putchr(' ');
 }
 
 //PRINTS / STR
@@ -51,7 +50,7 @@ int ft_putstr(char *str)
 	{
 		if (*ptr && *ptr != '\0')
 		{
-			write(1, ptr, 1);
+			ft_putchr(*ptr);
 		}
 		++ptr;
 	}
@@ -63,14 +62,15 @@ int ft_putstr(char *str)
 
 int printf_c (va_list args, int w, int left_align, int idx)
 {
+	int arg;
+
+	arg = va_arg(args, int);
+	
 	if (w > 0 && left_align == 0)
-	{
-		align(idx, w);
-	}
-	idx = ft_putchr(args);
+		align(w);
+	idx = ft_putchr(arg);
 	if (w > 0 && left_align == 1)
-		if (w > 0)
-			align(idx, w);
+		align(w);
 	return (idx);
 }
 
@@ -78,31 +78,28 @@ int printf_s (va_list args, int w, int left_align, int idx)
 {
 	int len;
 	char *arg;
-	char *aux;
 
-	aux = NULL;
 	len = 0;
 	arg = va_arg(args, char *);
-	aux = arg;
+
 	if (w > 0 && left_align == 0)
 	{
 		len = ft_strlen(arg);
-		w = w - len;
+		w = (w + 1) - len;
 		if (w > 0)
-			align(idx, w);
+			align(w);
 	}
-	idx = ft_putstr(aux);
+	idx = ft_putstr(arg);
 	if (w > 0 && left_align == 1)
 	{
-		len = ft_strlen(aux);
-		w = w - len;
+		len = ft_strlen(arg);
+		w = (w + 1) - len;
 		if (w > 0)
-			align(idx, w);
+			align(w);
 	}
+
 	return (idx);
 }
-
-
 
 // CHAR %c
 int int_size(int i)
@@ -248,43 +245,45 @@ int ft_printf(const char *format, ...)
 	return (done);
 }
 
-/*/
+//
 int main (void)
 {
-	printf("\n-----//TEST 1\n-------------------\n");
-	printf("\nidx: %i\n", printf("%s", ""));
-	printf("\nidx: %i\n", ft_printf("%s", ""));
-	printf("\n-----//TEST 2\n-------------------\n");
-	printf("\nidx: %i\n", printf("%s", "0"));
-	printf("\nidx: %i\n", ft_printf("%s", "0"));
-	printf("\n-----//TEST 3\n-------------------\n");
-	// Empty values cause over-counting; Add strlen()
-	printf("\nidx: %i\n", printf("%s %s", "", ""));
-	printf("\nidx: %i\n", ft_printf("%s %s", "", ""));
-	printf("\n-----//TEST 4\n-------------------\n");
-	printf("\nidx: %i\n", printf("%s %s", "0", "1"));
-	printf("\nidx: %i\n", ft_printf("%s %s", "0", "1"));
-	printf("\n-----//TEST 5\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %s %s ", "0", "1"));
-	printf("\nidx: %i\n", ft_printf(" %s %s ", "0", "1"));
-	printf("\n-----//TEST 6\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %s %s ", "", ""));
-	printf("\nidx: %i\n", ft_printf(" %s %s ", "", ""));
-	printf("\n-----//TEST 7\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %1s %1s ", "123", "4567"));
-	printf("\nidx: %i\n", ft_printf(" %1s %1s ", "123", "4567"));
-	printf("\n-----//TEST 8\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %4s %4s ", "123", "4567"));
-	printf("\nidx: %i\n", ft_printf(" %4s %4s ", "123", "4567"));
-	printf("\n-----//TEST 9\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %-4s %4s ", "123", "4567"));
-	printf("\nidx: %i\n", ft_printf(" %-4s %4s ", "123", "4567"));
-	printf("\n-----//TEST 10\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %4s %-4s", "123", "4567"));
-	printf("\nidx: %i\n", ft_printf(" %4s %-4s", "123", "4567"));
-	printf("\n-----//TEST 11\n-------------------\n");
-	printf("\nidx: %i\n", printf(" %-4s %-4s", "123", "4567"));
-	printf("\nidx: %i\n", ft_printf(" %-4s %-4s", "123", "4567"));
-	
+	//printf("\n-----//TEST 1\n-------------------\n");
+	//printf("\nidx: %i\n", printf("%s", ""));
+	//printf("\nidx: %i\n", ft_printf("%s", ""));
+	//printf("\n-----//TEST 2\n-------------------\n");
+	//printf("\nidx: %i\n", printf("%s", "0"));
+	//printf("\nidx: %i\n", ft_printf("%s", "0"));
+	//printf("\n-----//TEST 3\n-------------------\n");
+	//// Empty values cause over-counting; Add strlen()
+	//printf("\nidx: %i\n", printf("%s %s", "", ""));
+	//printf("\nidx: %i\n", ft_printf("%s %s", "", ""));
+	//printf("\n-----//TEST 4\n-------------------\n");
+	//printf("\nidx: %i\n", printf("%s %s", "0", "1"));
+	//printf("\nidx: %i\n", ft_printf("%s %s", "0", "1"));
+	//printf("\n-----//TEST 5\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %s %s ", "0", "1"));
+	//printf("\nidx: %i\n", ft_printf(" %s %s ", "0", "1"));
+	//printf("\n-----//TEST 6\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %s %s ", "", ""));
+	//printf("\nidx: %i\n", ft_printf(" %s %s ", "", ""));
+	//printf("\n-----//TEST 7\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %1s %1s ", "123", "4567"));
+	//printf("\nidx: %i\n", ft_printf(" %1s %1s ", "123", "4567"));
+	//printf("\n-----//TEST 8\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %4s %4s ", "123", "4567"));
+	//printf("\nidx: %i\n", ft_printf(" %4s %4s ", "123", "4567"));
+	//printf("\n-----//TEST 9\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %-4s %4s ", "123", "4567"));
+	//printf("\nidx: %i\n", ft_printf(" %-4s %4s ", "123", "4567"));
+	//printf("\n-----//TEST 10\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %4s %-4s", "123", "4567"));
+	//printf("\nidx: %i\n", ft_printf(" %4s %-4s", "123", "4567"));
+	//printf("\n-----//TEST 11\n-------------------\n");
+	//printf("\nidx: %i\n", printf(" %-4s %-4s", "123", "4567"));
+	//printf("\nidx: %i\n", ft_printf(" %-4s %-4s", "123", "4567"));
+	printf("\n-----//TEST 16\n-------------------\n");
+	printf("\nidx: %i\n", printf(" %*.s %.1s ", 10, "123", "4567"));
+	printf("\nidx: %i\n", ft_printf(" %*.s %.1s ", 10, "123", "4567"));
 }
-/*/
+//
--- printf.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTF_H
-
-# define PRINTF_H
-
-# include <unistd.h>
-# include <stdio.h>
-# include <stdarg.h>
-
-#endif
