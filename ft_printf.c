8b382cff0cbf01aac41860fb015db8f40fd1f56d
marymota <dg.marciamota@gmail.com>
2021-05-08 21:04:48 +0100

    Commented all funcions; Mixed repetitive functions (printf_width() substitutes printf_precision(); Divided parse_format() to reduce its size

--- ft_printf.c
+++ ft_printf.c
@@ -14,8 +14,8 @@ printf() is called with a "format string" that specifies how to format the value
 */
 
 //	*** INIT VARIABLES ***
-//	Iniciate variables that are used 
-//	globally to their 'default' values
+
+//	Iniciate variables that are used globally to their 'default' values
 void init(t_flags *flag)
 {
 	flag->width = 0;
@@ -25,14 +25,14 @@ void init(t_flags *flag)
 }
 
 /*	*** PRINTS ***
+
 	Prints individual characters	*/
 int ft_putchr(char c)
 {
 	return (write(1, &c, 1));
 }
 	
-/*	Prints spaces to align content based on 
-	width, precision & alignement	*/
+/*	Prints spaces to align content based on width, precision & alignement	*/
 void align(int len)
 {
 	while (len--)
@@ -64,9 +64,10 @@ int ft_putstr(char *str)
 	return (len);
 }
 
-int pft_print_prec(char *arg, t_flags *flag, int size)
+/*	Prints the extra width after subtracting the content length */
+int printf_width(char *arg, t_flags *flag, int size)
 {
-	int idx;
+	int	idx;
 
 	idx = 0;
 	if (flag->width == 0)
@@ -86,7 +87,8 @@ int pft_print_prec(char *arg, t_flags *flag, int size)
 	return (idx);
 }
 
-int pft_truncate(char *arg, t_flags *flag)
+/*	If precision is less than the length of the string, the string will be truncate */
+int printf_truncate(char *arg, t_flags *flag)
 {
 	int idx;
 	int count_precision;
@@ -120,25 +122,25 @@ int pft_truncate(char *arg, t_flags *flag)
 	return (idx);
 }
 
-/*	Prints the extra width after 
-subtracting the content length */
-int print_width (char *arg, t_flags *flag)
+/*	Prints the extra width after subtracting the content length */
+int parse_width (char *arg, t_flags *flag)
 {
-	int	i;
+	int	idx;
 	int	len;
 
-	i = 0;
+	idx = 0;
 	len = ft_strlen(arg);
 	flag->width = flag->width - len;
 	if (flag->width > 0)
 	{
-		i = flag->width;
+		idx = flag->width;
 		align(flag->width);
 	}
-	return (i);
+	return (idx);
 }
 
-int print_precision (char *arg, t_flags *flag)
+/* Returns the necessary extra width space when precision is used*/
+int parse_precision (char *arg, t_flags *flag)
 {
 	int idx;
 	int len;
@@ -151,37 +153,29 @@ int print_precision (char *arg, t_flags *flag)
 	precision = flag->precision;
 	width = flag->width;
 	size = 0;
-	/*	If precision is less than the length
-	of the string, the string will be truncate */
 	if (precision < len)
-	{
-		idx = pft_truncate(arg, flag);
-	}
+		idx = printf_truncate(arg, flag);
 	else if (precision >= len)
 	{
 		if (precision < width)
-		{
 			size = width - len;
-		}
 		else if (precision >= width)
-		{
 			size = precision - len;
-		}
 		else if (precision == 0)
 			align(width);
-		idx = pft_print_prec(arg, flag, size);
+		idx = printf_width(arg, flag, size);
 	}
 	return (idx);
 }
 
-//PRINTS / PRINT_SPECIFIERS
+//	PRINTS / PRINT_SPECIFIERS
 
+/*	Prints and aligns one character	*/
 int printf_c (va_list args, t_flags *flag, int idx)
 {
-	int arg;
+	int	arg;
 
 	arg = va_arg(args, int);
-	
 	if (flag->width > 1 && flag->left_align == 0)
 		align(flag->width - 1);
 	idx = ft_putchr(arg);
@@ -190,83 +184,56 @@ int printf_c (va_list args, t_flags *flag, int idx)
 	return (idx);
 }
 
+/*	Processes the printing of strings	*/
 int printf_s (va_list args, t_flags *flag, int idx)
 {
-	char *arg;
-	int aux = 0;
-	//int len = 0;
+	char	*arg;
 
 	arg = va_arg(args, char *);
-
 	if (flag->precision >= 0)
-	{
-		//printf("\ntest: %i\n", aux);
-		aux += print_precision(arg, flag);
-	}
+		idx += parse_precision(arg, flag);
 	else 
 	{
 		if (flag->width > 0 && flag->left_align == 0)
-			aux += print_width(arg, flag);
-		idx = ft_putstr(arg);
+			idx += parse_width(arg, flag);
+		idx += ft_putstr(arg);
 		if (flag->width > 0 && flag->left_align == 1)
-			aux += print_width(arg, flag);
+			idx += parse_width(arg, flag);
 	}
-	return (idx + aux);
+	return (idx);
 }
 
-// CHAR %c
+/* Counts digits of numeric values	*/
 int int_size(int i)
 {
-	int ret;
+	int	idx;
 
-	ret = 0;
+	idx = 0;
 	if (i == 0)
 		return 1; //If it returns 0 it can't advance when the digit is '0'
 	if (!(i / 10))
 		return 1;
 	else
-		++ret;
-	++ret;
-	return (ret);
+		++idx;
+	++idx;
+	return (idx);
 }
 
 
-//CHECKS
+//	CHECKS
+
+/* Checks is a character is a digit*/
 int is_digit(const char *format)
 {
 	return (*format >= 48 && *format <= 57);
 }
 
-int check_left(const char *format)
+/* Returns a number from a combination of digit characters	*/
+int count_width(const char *format, va_list args, t_flags *flag)
 {
-	if (*format == '-')
-	{
-		return (1);
-	}
-	return (0);
-}
+	int ret;
 
-int check_asterisk(const char *format)
-{
-	if (*format == '*')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int check_precision(const char *format)
-{
-	if (*format == '.')
-	{
-		return (0);
-	}
-	return (-1);
-}
-
-int count_width(const char *format, t_flags *flag, va_list args)
-{
-	flag->width = 0;
+	ret = 0;
 	if (flag->asterisk == 1)
 	{
 		flag->asterisk = va_arg(args, int);
@@ -276,37 +243,17 @@ int count_width(const char *format, t_flags *flag, va_list args)
 	{
 		while (is_digit(format))
 		{
-			flag->width = flag->width * 10 + (*format - '0');
-			format++;
-		}
-	}
-	return (flag->width);
-}
-
-int count_precision(const char *format, t_flags *flag, va_list args)
-{
-	flag->precision = 0;
-	if (flag->asterisk == 1)
-	{
-		flag->asterisk = va_arg(args, int);
-		return (flag->asterisk);
-	}
-	if (is_digit(format))
-	{
-		while (is_digit(format))
-		{
-			flag->precision = flag->precision * 10 + (*format - '0');
-			//printf("\ntest: %i \n", flag->precision);
+			ret = ret * 10 + (*format - '0');
 			++format;
 		}
 	}
-	//printf("\ntest: %i \n", flag->precision);
-	return (flag->precision);
+	return (ret);
 }
 
-int get_spec(const char *format, va_list args, t_flags *flag)
+/* Finds the specifier and sends it to be print	*/
+int get_specifier(const char *format, va_list args, t_flags *flag)
 {
-	int idx;
+	int	idx;
 	
 	idx = 0;
 	if (*format == 'c')
@@ -323,7 +270,60 @@ int get_spec(const char *format, va_list args, t_flags *flag)
 }
 
 
+/*	Defines the value for precision and width if the asterisk flag is used  */
+void parse_asterisk(const char *format, va_list args, t_flags *flag)
+{
+	if (flag->precision >= 0) // If default value is negative don't forget to compare...
+		flag->precision = count_width(format, args, flag);
+	else
+	{
+		flag->width = count_width(format, args, flag);
+		flag->asterisk = 0;
+	}
+	if (flag->width < 0)
+	{
+		flag->width = flag->width * -1;
+		flag->left_align = 1;
+	}
+}
 
+/* Find and update flags value 	*/
+int get_flags(const char *format, va_list args, t_flags *flag)
+{
+	if (*format == '-')
+		flag->left_align = 1;
+	else if (*format == '*')
+	{
+		flag->asterisk = 1;
+		parse_asterisk(format, args, flag);
+	}
+	else if (*format == '.')
+		flag->precision = 0;
+	else	
+		return (0);
+	return (1);
+}
+
+/* Advances format after counting width and/or precision	*/
+int count_digits(const char *format, va_list args, t_flags *flag)
+{
+	int fmt;
+
+	fmt = 0;
+	if (flag->precision >= 0) // If default value is negative don't forget to compare...
+	{
+		flag->precision = count_width(format, args, flag);
+		fmt =  int_size(flag->precision);
+	}
+	else 
+	{
+		flag->width = count_width(format, args, flag);
+		fmt = int_size(flag->width);
+	}
+	return (fmt);
+}
+
+/*	Parse the format string to find flags width, precision & specifier	*/
 int parse_format(const char *format, va_list args, t_flags *flag, int idx)
 {
 	init(flag);
@@ -334,75 +334,24 @@ int parse_format(const char *format, va_list args, t_flags *flag, int idx)
 			++format;
 			while (*format == '-' || *format == '*' || *format == '.' || is_digit(format))
 			{
-				if (*format == '-')
-				{
-					flag->left_align = check_left(format);
+				if (get_flags(format, args, flag))
 					++format;
-				}	
-				else if (*format == '*')
-				{
-					flag->asterisk = check_asterisk(format);
-					
-					if (flag->precision >= 0) // If default value is negative don't forget to compare...
-						flag->precision = count_width(format, flag, args);
-					else
-					{
-						flag->width = count_width(format, flag, args);
-						flag->asterisk = 0;
-						
-					}
-					if (flag->width < 0)
-					{
-						flag->width = flag->width * -1;
-						flag->left_align = 1;
-					}
-					++format;
-				}
 				else if (is_digit(format))
-				{
-					if (flag->precision >= 0) // If default value is negative don't forget to compare...
-					{
-						flag->precision = count_precision(format, flag, args);
-						if (flag->precision < 0)
-						{
-							flag->precision = flag->precision * -1;
-							flag->left_align = 1;
-						}
-						format += int_size(flag->precision);
-					}
-					else 
-					{
-						flag->width = count_width(format, flag, args);
-						if (flag->width < 0)
-						{
-							flag->width = flag->width * -1;
-							flag->left_align = 1;
-						}
-						format += int_size(flag->width);
-					}
-
-				}
-				else if (*format == '.')
-				{
-					flag->precision = check_precision(format);
-					++format;
-					//flag->precision = count_precision(format, flag, args);
-					//printf("\ntest: %i %s \n", flag->precision, format);
-				}
+					format += count_digits(format, args, flag);
 			}
-			idx += get_spec(format, args, flag);
+			idx += get_specifier(format, args, flag);
 			init(flag);
 		}
 		else
 		{
-			write(1, format, 1);
-			++idx;
+			idx += ft_putchr(*format);
 		}                
 		++format;
 	}
 	return (idx);
 }
 
+/*	Iniciates a list of variadic arguments and sends the format string to be parsed*/
 int ft_printf(const char *format, ...)
 {
 	va_list args;
@@ -413,7 +362,6 @@ int ft_printf(const char *format, ...)
 	idx = 0;
 	va_start(args, format);
 	done = parse_format(format, args, &flag, idx);
-
 	va_end (args);
 	return (done);
 }
