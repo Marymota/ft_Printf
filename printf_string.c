d5c5b7eb3bc34c16fbd3bf5f7a7cd104d42b9106
marymota <dg.marciamota@gmail.com>
2021-06-04 19:30:16 +0100

    changing %s functions

--- printf_string.c
+++ printf_string.c
@@ -1,64 +1,5 @@
 #include "ft_printf.h"
 
-void	printf_precision(char *arg, t_flags *flag, int size)
-{
-	if (flag->width == 0)
-		ft_putstr_fd(arg, 1);
-	else if (flag->left_align == 0)
-	{
-		if (size > 0)
-			align(size);
-		ft_putstr_fd(arg, 1);
-	}
-	else if (flag->left_align == 1)
-	{
-		ft_putstr_fd(arg, 1);
-		if (size > 0)
-			align(size);
-	}
-}
-
-int	return_string(t_flags *flag, int idx, int len)
-{
-	idx = len;
-	if (flag->width <= flag->precision)
-	{
-		if (flag->precision >= 0 && flag->precision < len)
-			idx = flag->precision;
-	}
-	else if (flag->width == 0)
-	{
-		if (flag->precision == -1)
-		{
-			if (len == -1)
-				idx = 0;
-		}
-	}
-	else if (flag->width >= 0)
-	{
-		if (flag->precision >= 0 || flag->width >= len)
-			idx = flag->width;
-	}
-	return (idx);
-}
-
-int	parse_width_string (char *arg, t_flags *flag)
-{
-	int	idx;
-	int	len;
-	int	size;
-
-	idx = 0;
-	len = ft_strlen(arg);
-	size = flag->width - len;
-	if (size > 0)
-	{
-		idx = size;
-		align(size);
-	}
-	return (idx);
-}
-
 int	parse_precision_string (char *arg, t_flags *flag)
 {
 	int	idx;
@@ -70,18 +11,18 @@ int	parse_precision_string (char *arg, t_flags *flag)
 	size = 0;
 	if (flag->precision < len)
 		idx = printf_truncate(arg, flag);
-	else if (flag->precision >= len)
+	else if (flag->precision >= len && flag->left_align == 1)
 	{
 		if (flag->precision < flag->width)
 			size = flag->width - len;
-		else if (flag->precision >= flag->width)
+		else if (flag->precision >= flag->width && flag->width > len)
 			size = flag->precision - len;
 		else if (flag->precision == 0)
 			align(flag->width);
 		printf_precision(arg, flag, size);
 	}
 	return (idx);
-}
+} 
 
 int	printf_truncate(char *arg, t_flags *flag)
 {
@@ -107,3 +48,68 @@ int	printf_truncate(char *arg, t_flags *flag)
 	}
 	return (idx);
 }
+
+void	printf_precision(char *arg, t_flags *flag, int size)
+{
+	if (flag->width == 0)
+		ft_putstr_fd(arg, 1);
+	else if (flag->left_align == 0)
+	{
+		if (size > 0)
+			align(size);
+		ft_putstr_fd(arg, 1);
+	}
+	else if (flag->left_align == 1)
+	{
+		ft_putstr_fd(arg, 1);
+		if (size > 0)
+			align(size);
+	}
+}
+
+int	parse_width_string (char *arg, t_flags *flag)
+{
+	int	idx;
+	int	len;
+	int	size;
+
+	idx = 0;
+	len = ft_strlen(arg);
+	size = flag->width - len;
+	if (size > 0)
+	{
+		idx = size;
+		align(size);
+	}
+	return (idx);
+}
+
+int	return_string(t_flags *flag, int idx, int len)
+{
+	idx = len;
+	if (flag->width <= flag->precision)
+	{
+		if (flag->precision >= 0 && flag->precision <= len)
+			idx = flag->precision;
+		else if (flag->precision > len && flag->width > len)
+			idx = flag->width;
+		else if (flag->width < len)
+			idx = len;
+	}
+	else if (flag->width == 0)
+	{ 
+		if (flag->precision == -1)
+		{
+			if (len == -1)
+				idx = 0;
+		}
+	}
+	else if (flag->width >= 0)
+	{
+		if (flag->precision >= 0 || flag->width >= len)
+			idx = flag->width;
+	}
+	return (idx);
+}
+ 
+  
